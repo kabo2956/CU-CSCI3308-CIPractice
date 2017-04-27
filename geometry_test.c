875c0f6c3f43c2dda5956084607d36416bdd12ef
Kaleb Bodisch <kabo2956@colorado.edu>
2017-04-26 19:19:06 -0600

    added triangle and three tests.

--- geometry_test.c
+++ geometry_test.c
@@ -15,6 +15,7 @@
 
 #include "geometry.h"
 
+
 /* coord_2d_eq Test */
 START_TEST(test_2d_eq)
 {
@@ -50,7 +51,6 @@ START_TEST(test_2d_eq)
     b.x = 7.77;
     b.y = 8.88;
     ck_assert(!coord_2d_eq(&a, &b));
-
 }
 END_TEST
 
@@ -145,6 +145,38 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+/* coord_2d_area_triangle test */
+START_TEST(tc_coord_2d_area_triangle){
+	coord_2d_t a;
+	coord_2d_t b;
+	coord_2d_t c;
+	
+	a.x = 0;
+	a.y = 1;
+	b.x = 0;
+	b.y = 0;
+	c.x = 1;
+	c.y = 0;
+	ck_assert(abs(coord_2d_area_triangle(&a, &b, &c)-0.5) < 0.01);
+	
+	a.x = 0;
+	a.y = 0;
+	b.x = 2;
+	b.y = 2;
+	c.x = 4;
+	c.y = -4;
+	ck_assert(abs(coord_2d_area_triangle(&a, &b, &c)-8) < 0.01 );
+
+	a.x = 0;
+	a.y = 0;
+	b.x = 0;
+	b.y = 0;
+	c.x = 0;
+	c.y = 1;
+	ck_assert(abs(coord_2d_area_triangle(&a, &b, &c)) < 0.01);
+}
+END_TEST
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,11 +194,13 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase *tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, tc_coord_2d_area_triangle);
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-
+    suite_add_tcase(s, tc_2d_area_triangle);
     /* Return Suite */
     return s;
 
